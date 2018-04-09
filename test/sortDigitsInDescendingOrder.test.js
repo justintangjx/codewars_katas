cd6793b8ec84b2686c264b2d628b080818e4ea20
“justintangjx” <just.think.tang@gmail.com>
2018-04-09 10:09:14 +0800

    findDigit kata

--- test/sortDigitsInDescendingOrder.test.js
+++ test/sortDigitsInDescendingOrder.test.js
@@ -1,26 +1,26 @@
-const sortDigitsInDescendingOrder = require("../src/sortDigitsInDescendingOrder");
+// const sortDigitsInDescendingOrder = require("../src/sortDigitsInDescendingOrder");
 
-describe("sorting digits", () => {
-  it("should behave...", () => {
-    expect(sortDigitsInDescendingOrder(21445)).toEqual(54421);
-  });
-  it("should return 654321", () => {
-    expect(sortDigitsInDescendingOrder(145263)).toEqual(654321);
-  });
+// describe("sorting digits", () => {
+//   it("should behave...", () => {
+//     expect(sortDigitsInDescendingOrder(21445)).toEqual(54421);
+//   });
+//   it("should return 654321", () => {
+//     expect(sortDigitsInDescendingOrder(145263)).toEqual(654321);
+//   });
 
-  it("should return 9875543221", () => {
-    expect(sortDigitsInDescendingOrder(1254859723)).toEqual(9875543221);
-  });
+//   it("should return 9875543221", () => {
+//     expect(sortDigitsInDescendingOrder(1254859723)).toEqual(9875543221);
+//   });
 
-  it("should return 98755432210", () => {
-    expect(sortDigitsInDescendingOrder(12548059723)).toEqual(98755432210);
-  });
+//   it("should return 98755432210", () => {
+//     expect(sortDigitsInDescendingOrder(12548059723)).toEqual(98755432210);
+//   });
 
-  it("should return 0", () => {
-    expect(sortDigitsInDescendingOrder(0)).toEqual(0);
-  });
+//   it("should return 0", () => {
+//     expect(sortDigitsInDescendingOrder(0)).toEqual(0);
+//   });
 
-  it("should return nothing", () => {
-    expect(sortDigitsInDescendingOrder()).toEqual(0);
-  });
-});
+//   it("should return nothing", () => {
+//     expect(sortDigitsInDescendingOrder()).toEqual(0);
+//   });
+// });
